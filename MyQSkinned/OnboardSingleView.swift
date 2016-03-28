5858a7c270db210610ba00202a76b7460d4ce98c
Maury Alamin <mauryalamin@me.com>
2016-03-28 14:39:51 -0500

    * Changed named textfield keyboard on Create Account view to Capital Words * Added Onboarding View
    To Do
    * Add NavigationViewControllers to Create Account -> Verify views
    * Places View

---
+++ MyQSkinned/OnboardSingleView.swift
@@ -0,0 +1,54 @@
+//
+//  OnboardSingleView.swift
+//  MyQSkinned
+//
+//  Created by Maury Alamin on 3/27/16.
+//  Copyright © 2016 Alamin. All rights reserved.
+//
+
+import UIKit
+
+class OnboardSingleView: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var backgroundImage: UIImage?
+    var titleString: String?
+    var descriptionString: String?
+    
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        backgroundImageView.image = backgroundImage
+        
+        if let titleString = titleString {
+            let font = UIFont(name: "OpenSans-Light", size:34.0)!
+            let paragraphStyle = NSMutableParagraphStyle()
+            // paragraphStyle.lineSpacing = 8
+            paragraphStyle.alignment = .Center
+            
+            titleLabel.attributedText = NSAttributedString(string: titleString,
+                                                          attributes: [NSFontAttributeName: font, NSParagraphStyleAttributeName: paragraphStyle])
+        }
+        
+        if let descriptionString = descriptionString {
+            let font = UIFont(name: "OpenSans", size:16.0)!
+            let paragraphStyle = NSMutableParagraphStyle()
+            // paragraphStyle.lineSpacing = 0
+            paragraphStyle.alignment = .Left
+            
+            descriptionLabel.attributedText = NSAttributedString(string: descriptionString,
+                                                           attributes: [NSFontAttributeName: font, NSParagraphStyleAttributeName: paragraphStyle])
+        }
+        
+    }
+
+
+    
+
+}
---
+++ MyQSkinned/OnboardView.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardView.swift
+//  MyQSkinned
+//
+//  Created by Maury Alamin on 3/27/16.
+//  Copyright © 2016 Alamin. All rights reserved.
+//
+
+import UIKit
+
+class OnboardView: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var pages = [OnboardSingleView]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        scrollView.pagingEnabled = true
+        
+        let page1 = createAndAddOnboardView("onboard1", titleText: "Smarter is Better", descriptionText: "With MyQ, stay connected and control your home – from anywhere, at anytime.")
+        let page2 = createAndAddOnboardView("onboard2", titleText: "Always informed", descriptionText: "Know if your garage door opens or if you forgot to close it.\r\rHistory reveals detailed event activity for your devices. ")
+        let page3 = createAndAddOnboardView("onboard3", titleText: "More than garage door openers", descriptionText: "Control of a variety of devices, like remote light controls and switches.\r\rAdd even more devices from our partners with Account Linking.")
+        let page4 = createAndAddOnboardView("onboard4", titleText: "Added Security", descriptionText: "Rest assured knowing only authorized users can open your garage doors, when you add a pin code to your door opener. ")
+        let page5 = createAndAddOnboardView("onboard5", titleText: "Peace of mind in the palm of your hand", descriptionText: "Stay connected and in control with MyQ.")
+        
+        pages = [page1, page2, page3, page4, page5]
+        pageControl.numberOfPages = pages.count
+        
+        let views = ["view": view, "page1": page1.view, "page2": page2.view, "page3": page3.view, "page4": page4.view, "page5": page5.view]
+        
+        let metrics = ["edgeMargin": 10, "betweenMargin": 20]
+        
+        let verticalContraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[page1(==view)]|", options: [], metrics: nil, views: views)
+        NSLayoutConstraint.activateConstraints(verticalContraints)
+        
+        let horizontalContraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-edgeMargin-[page1(==view)]-betweenMargin-[page2(==view)]-betweenMargin-[page3(==view)]-betweenMargin-[page4(==view)]-betweenMargin-[page5(==view)]-edgeMargin-|", options: [.AlignAllTop, .AlignAllBottom], metrics: metrics, views: views)
+        NSLayoutConstraint.activateConstraints(horizontalContraints)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func createAndAddOnboardView(backgroundImageName: String, titleText: String, descriptionText: String) -> OnboardSingleView {
+        let onboardView = storyboard!.instantiateViewControllerWithIdentifier("OnboardSingleView") as! OnboardSingleView
+        onboardView.view.translatesAutoresizingMaskIntoConstraints = false
+        onboardView.backgroundImage = UIImage(named: backgroundImageName)
+        onboardView.titleString = titleText
+        onboardView.descriptionString = descriptionText
+        
+        scrollView.addSubview(onboardView.view)
+        
+        addChildViewController(onboardView)
+        onboardView.didMoveToParentViewController(self)
+        
+        // print("Background Image: \(onboardView.backgroundImage), Title Text: \(onboardView.titleString), Description Text: \(onboardView.descriptionString)")
+        
+        return onboardView
+    }
+    
+    @IBAction func dismissView(sender: UIButton) {
+        
+        self.dismissViewControllerAnimated(true, completion: {})
+        
+    }
+
+}
+
+extension OnboardView: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let pageWidth = CGRectGetWidth(scrollView.bounds)
+        let pageFraction = scrollView.contentOffset.x / pageWidth
+        
+        pageControl.currentPage = Int(round(pageFraction))
+        
+        // print("\(pageWidth), \(pageFraction)")
+    }
+    
+}
+
+
+
+
+
