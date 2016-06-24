e23230d397f9a7020b34b6abd540cc231afa601b
Maury Alamin <mauryalamin@me.com>
2016-06-24 12:10:36 -0500

    Revised Launch and Places wallpaper Added 3D Touch Application Shortcut functionality (not hooked up)

--- MyQSkinned/AppDelegate.swift
+++ MyQSkinned/AppDelegate.swift
@@ -10,19 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var centerContainer: MMDrawerController?
-
-
+    
+    var viewIdentifier: String?
+    var viewController: UIViewController?
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         let isUserLoggedIn = NSUserDefaults.standardUserDefaults().boolForKey("isUserLoggedIn")
         
         if isUserLoggedIn {
-            
-            // var rootViewController = self.window!.rootViewController
             
             let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             
@@ -47,29 +48,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        
+        switch shortcutItem.type {
+        case "history":
+            viewIdentifier = "History"
+            viewController = History()
+        case "rules":
+            viewIdentifier = "Rules"
+            viewController = Rules()
+        default:
+            viewIdentifier = "PlacesView"
+            viewController = PlacesView()
+        }
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
